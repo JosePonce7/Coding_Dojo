84ce434b83fa4014db712faca49190345d3ff287
Jose Ponce <ponce900@gmail.com>
2022-04-05 18:52:46 -0500

    update

--- miprimerreact/my-first-app/src/App.js
+++ miprimerreact/my-first-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Header extends Component {
+    render() {
+        const {title} = this.props;
+        return (
+            <div className='header'>
+                <h1>Hello Dojo!</h1>
+                <h2>Things I need to do:</h2>
+                <li>
+                  React
+                </li>
+                <li>
+                  Mongo
+                </li>
+                <li>
+                  Surf
+                </li>
+                <li>
+                  Work
+                </li>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Header;
